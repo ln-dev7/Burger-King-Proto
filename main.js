1ac84a025cf09b9dc6266689150baf2f97e729c5
LN <leonelngoya@gmail.com>
2024-01-06 16:59:42 +0100

    second anim

--- main.js
+++ main.js
@@ -8,9 +8,6 @@ let variation = 1;
 
 arrowRight.addEventListener("click", () => {
   if (variation < 6) {
-    gsap.to(".arrow-left", {
-      opacity: 1,
-    });
     if (variation === 1) {
       gsap.to(".container-1 .burger-container img", {
         marginBottom: "-70px",
@@ -96,11 +93,59 @@ arrowRight.addEventListener("click", () => {
       gsap.to(".container-3 .burger-title", {
         y: -325,
       });
-      gsap.to(".arrow-right", {
-        opacity: 0,
+    }
+    // if (variation === 6) {
+    //   gsap.to("body", {
+    //     background: "#861511",
+    //   });
+    //   gsap.to(".container-2, .container-3", {
+    //     opacity: 0,
+    //     transform: "translateX(-50%) translateY(100%)",
+    //   });
+    //   gsap.to(".container-1", {
+    //     opacity: 1,
+    //     transform: "translateX(-50%) translateY(-50%)",
+    //   });
+    //   gsap.to(".dot-2, .dot-3", {
+    //     background: "transparent",
+    //   });
+    //   gsap.to(".dot-1", {
+    //     background: "white",
+    //   });
+    //   gsap.to(".blur-2, .blur-3", {
+    //     opacity: 0,
+    //   });
+    //   gsap.to(".blur-1", {
+    //     opacity: 1,
+    //   });
+    // }
+    variation++;
+  }
+  console.log("variation", variation);
+});
+
+arrowLeft.addEventListener("click", () => {
+  if (variation > 1) {
+    if (variation === 2) {
+      gsap.to(".container-1 .burger-container img", {
+        marginBottom: "0",
+        y: 0,
+      });
+      gsap.to(".container-1 .burger-title", {
+        y: 0,
       });
     }
-    if (variation === 6) {
+    if (variation === 3) {
+      gsap.to(".container-1 .burger-container img", {
+        marginBottom: "-70px",
+        y: 100,
+      });
+      gsap.to(".container-1 .burger-container img:nth-child(2)", {
+        marginBottom: "-100px",
+      });
+      gsap.to(".container-1 .burger-title", {
+        y: -325,
+      });
       gsap.to("body", {
         background: "#861511",
       });
@@ -125,28 +170,26 @@ arrowRight.addEventListener("click", () => {
         opacity: 1,
       });
     }
-  }
-  variation++;
-});
-
-arrowLeft.addEventListener("click", () => {
-  if (variation > 1) {
-    gsap.to(".arrow-right", {
-      opacity: 1,
-    });
-    if (variation === 1) {
-      gsap.to(".container-1 .burger-container img", {
+    if (variation === 4) {
+      gsap.to(".container-2 .burger-container img", {
+        marginBottom: "0",
+        y: 0,
+      });
+      gsap.to(".container-2 .burger-title", {
+        y: 0,
+      });
+    }
+    if (variation === 5) {
+      gsap.to(".container-2 .burger-container img", {
         marginBottom: "-70px",
         y: 100,
       });
-      gsap.to(".container-1 .burger-container img:nth-child(2)", {
+      gsap.to(".container-2 .burger-container img:nth-child(2)", {
         marginBottom: "-100px",
       });
-      gsap.to(".container-1 .burger-title", {
+      gsap.to(".container-2 .burger-title", {
         y: -325,
       });
-    }
-    if (variation === 2) {
       gsap.to("body", {
         background: "#0D722F",
       });
@@ -158,60 +201,29 @@ arrowLeft.addEventListener("click", () => {
         opacity: 1,
         transform: "translateX(-50%) translateY(-50%)",
       });
-    }
-    if (variation === 3) {
-      gsap.to(".container-2 .burger-container img", {
-        marginBottom: "-70px",
-        y: 100,
+      gsap.to(".dot-1, .dot-3", {
+        background: "transparent",
       });
-      gsap.to(".container-2 .burger-container img:nth-child(2)", {
-        marginBottom: "-100px",
+      gsap.to(".dot-2", {
+        background: "white",
       });
-      gsap.to(".container-2 .burger-title", {
-        y: -325,
-      });
-    }
-    if (variation === 4) {
-      gsap.to("body", {
-        background: "#C68300",
-      });
-      gsap.to(".container-1, .container-2", {
+      gsap.to(".blur-1, .blur-3", {
         opacity: 0,
-        transform: "translateX(-50%) translateY(100%)",
       });
-      gsap.to(".container-3", {
+      gsap.to(".blur-2", {
         opacity: 1,
-        transform: "translateX(-50%) translateY(-50%)",
-      });
-    }
-    if (variation === 5) {
-      gsap.to(".container-3 .burger-container img", {
-        marginBottom: "-70px",
-        y: 100,
-      });
-      gsap.to(".container-3 .burger-container img:nth-child(2)", {
-        marginBottom: "-100px",
-      });
-      gsap.to(".container-3 .burger-title", {
-        y: -325,
       });
     }
     if (variation === 6) {
-      gsap.to("body", {
-        background: "#861511",
+      gsap.to(".container-3 .burger-container img", {
+        marginBottom: "0",
+        y: 0,
       });
-      gsap.to(".container-2, .container-3", {
-        opacity: 0,
-        transform: "translateX(-50%) translateY(100%)",
-      });
-      gsap.to(".container-1", {
-        opacity: 1,
-        transform: "translateX(-50%) translateY(-50%)",
-      });
-      gsap.to(".arrow-left", {
-        opacity: 0,
+      gsap.to(".container-3 .burger-title", {
+        y: 0,
       });
     }
+    variation--;
   }
-  variation--;
+  console.log("variation", variation);
 });
